0603ac615288e0da4d0783c6da6bec5196389eee
harshsri28 <66912920+harshsri28@users.noreply.github.com>
2021-04-17 18:17:00 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div style={{ textAlign: "center", marginBottom: 10 }}>
+      Made With 💙 by{" "}
+      <a
+        href="https://www.linkedin.com/in/harsh-srivastava-a03747166"
+        style={{ cursor: "pointer" }}
+      >
+        Harsh Srivastava
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
